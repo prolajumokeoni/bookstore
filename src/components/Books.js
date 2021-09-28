d19dbe22026a320161a6f82b9cde44d46322dca8
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-09-28 10:23:18 +0100

    Add book and categories component

---
+++ src/components/Books.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Books = () => (
+
+  <div className="pt-5">
+    <h3>List of books</h3>
+    <button className="btn-dark btn-small rounded" type="button">Remove book</button>
+    <div className="d-flex pt-5">
+      <input type="text" placeholder="Add your book" />
+    </div>
+  </div>
+);
+
+export default Books;
