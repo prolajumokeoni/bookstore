fa610d165ff445b458b244ba9bcaf667531eab53
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-09-29 15:47:53 +0100

    Add and remove book function

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Addbooks from './Addbooks';
+import SingleBook from './SingleBook';
 
-const Books = () => (
-
-  <div className="pt-5 container">
-    <h3>List of books</h3>
-    <button className="btn-dark btn-small rounded" type="button">Remove book</button>
-    <div className="d-flex pt-5">
-      <input type="text" placeholder="Add your book" />
+const Books = () => {
+  const books = useSelector((state) => state.books);
+  return (
+    <div className="pt-5 container">
+      <h3>List of books</h3>
+      <ul className="list-group">
+        {books.length > 0 ? books.map((book) => <SingleBook key={book.id} book={book} />)
+          : 'No Books'}
+      </ul>
+      <Addbooks />
     </div>
-  </div>
-);
+  );
+};
 
 export default Books;
