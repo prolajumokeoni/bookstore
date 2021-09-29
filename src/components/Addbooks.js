200cbec2971b38f5df1834fece612b8ea9b62432
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-09-29 15:40:47 +0100

    Add new book component

---
+++ src/components/Addbooks.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addBooks } from '../redux/books/books';
+
+const Addbooks = () => {
+  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const addNewBook = (e) => {
+    if (title && category) {
+      e.preventDefault();
+      const data = {
+        title,
+        category,
+        id: books.length + 1,
+      };
+      dispatch(addBooks(data));
+      setTitle('');
+      setCategory('');
+    }
+  };
+
+  return (
+    <div>
+      <form>
+        <input className="form-control" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="book title" />
+        <input className="form-control" value={category} onChange={(e) => setCategory(e.target.value)} placeholder="category" />
+        <button onClick={addNewBook} type="button">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Addbooks;
