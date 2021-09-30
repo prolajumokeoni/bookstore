806e9dcf27eb51bfef7c1dd668d77b08edc8b73e
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-09-30 09:40:09 +0100

    Add boooklist to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => (
   <ul className="nav">
     <li className="nav-item">
-      <NavLink className="nav-link" to="/" exact>Books</NavLink>
+      <NavLink className="nav-link" to="/Booklists" exact>BOOKSTORE CMS</NavLink>
     </li>
     <li className="nav-item">
-      <NavLink className="nav-link" to="/books" exact>Books</NavLink>
+      <NavLink className="nav-link" to="/books" exact>New books</NavLink>
     </li>
     <li className="nav-item">
       <NavLink className="nav-link" to="/categories" exact>Categories</NavLink>
