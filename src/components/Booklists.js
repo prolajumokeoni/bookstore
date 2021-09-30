d7e106e5e01638ea3be0eff584fce16168c7d390
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-09-30 20:41:50 +0100

    Add styles to container for book lists and form

--- src/components/Booklists.js
+++ src/components/Booklists.js
@@ -12,8 +12,7 @@ const Booklists = () => {
   }, []);
   const books = useSelector((state) => state.books);
   return (
-    <div>
-      <h3>List of books</h3>
+    <div className="p-2">
       <ul className="list-group">
         {books.length > 0 ? books.map((book) => <SingleBook key={book.item_id} book={book} />)
           : 'No Books'}
--- src/components/SingleBook.js
+++ src/components/SingleBook.js
@@ -6,10 +6,10 @@ const SingleBook = ({ book }) => {
 
   return (
     <div>
-      <li className="list-group-item d-flex justify-content-between align-items-start">
+      <li className="list-group-item d-flex justify-content-between align-items-start m-2 shadow">
         <div className="ms-2 me-auto">
-          <div className="fw-bold">{book.title}</div>
-          {book.author}
+          <div className="fw-bolder">{book.title}</div>
+          {book.category}
         </div>
         <button type="button" className="btn btn-sm btn-link" onClick={() => dispatch(removeBooks(book.item_id))}>Remove Book</button>
       </li>
