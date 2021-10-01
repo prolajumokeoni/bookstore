7df55b3e70f71b536f94c10c9c1da7d00733c416
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-10-01 08:12:38 +0100

    Add progress bar

--- src/components/SingleBook.js
+++ src/components/SingleBook.js
@@ -10,10 +10,22 @@ const SingleBook = ({ book }) => {
         <div className="">
           <p className="text-muted">{book.category}</p>
           <div className="fw-bolder fs-3">{book.title}</div>
-          <button type="button" className="btn btn-sm btn-link " onClick={() => dispatch(removeBooks(book.item_id))}>Remove Book</button>
+          <button
+            type="button"
+            className="btn btn-sm btn-link "
+            onClick={() => dispatch(removeBooks(book.item_id))}
+          >
+            Remove Book
+          </button>
         </div>
         <div className="d-flex">
-          <div className="Oval-2" />
+          <div className="progress-circle over50 p69">
+            <span>69%</span>
+            <div className="left-half-clipper">
+              <div className="first50-bar" />
+              <div className="value-bar" />
+            </div>
+          </div>
           <div className="pt-2">
             <p>65%</p>
             <p className="text-muted">Completed</p>
@@ -22,7 +34,9 @@ const SingleBook = ({ book }) => {
         <div>
           <p className="text-muted">Current Chapter</p>
           <p>Chapter 17</p>
-          <p className="bg-primary rounded px-3 py-2 text-white">Update progress</p>
+          <p className="bg-primary rounded px-3 py-2 text-white">
+            Update progress
+          </p>
         </div>
       </li>
     </div>
